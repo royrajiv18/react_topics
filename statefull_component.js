9411f250dd23272a070e7d9147876504491670bc
Rajiv Roy <rajivpune18@gmail.com>
2025-03-04 12:42:45 +0530

    statefull and stateless component

---
+++ statefull_component.js
@@ -0,0 +1,16 @@
+// Statefull component
+
+import React, { useState } from "react";
+
+const Counter = () => {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <p>count: {count}</p>
+      <button onClick={() => setCount(count + 1)}>Increment</button>
+    </div>
+  );
+};
+
+export default Counter;
