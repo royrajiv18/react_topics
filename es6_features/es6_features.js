0cda8b3518e366ed03445d1873edd9f25ba29708
Rajiv Roy <rajivpune18@gmail.com>
2025-03-05 14:18:10 +0530

    es 6 plus more features

--- es6_features/es6_features.js
+++ es6_features/es6_features.js
@@ -220,3 +220,30 @@ console.log(set.has("name")); // Output: true
 
 // converting set to array -
 const myArray = Array.from(mySet);
+
+//****************************************** For/Of loop***********************
+//The for/of loop allows you to iterate over iterable objects like arrays, strings, Maps, and Sets
+//but in a short syntax as compared to other loops.
+
+// in array
+const a = ["apple", "banana", "cherry"];
+for (const fruit of a) {
+  console.log(fruit);
+}
+/*apple
+banana
+cherry
+*/
+
+// - in string
+const s = "hello";
+for (const char of s) {
+  console.log(char);
+}
+/*
+h
+e
+l
+l
+o
+*/
