b837007e3a2d438b0af909bc0a03ea3e6caa5b60
Rajiv Roy <rajivpune18@gmail.com>
2025-03-05 11:15:29 +0530

    useref example

---
+++ useRef.js
@@ -0,0 +1,24 @@
+/*
+The useRef hook returns a mutable object with a .current property that you can use to store a value. 
+Unlike useState, updating a useRef value does not trigger a component re-render. 
+*/
+
+import React, { useRef } from "react";
+
+function FocusInput() {
+  const inputRef = useRef(null);
+
+  const focusInput = () => {
+    inputRef.current.focus();
+  };
+
+  return (
+    <div>
+      <input ref={inputRef} type="text" />
+      <button onClick={focusInput}>Focus Input</button>
+    </div>
+  );
+}
+
+//In this example, the useRef hook is used to directly reference the input element,
+// allowing us to call the focus() method on it when the button is clicked.
